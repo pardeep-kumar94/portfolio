0a9eff95a75a2943afbddf8f585bf6d2eb702b59
pardeep-kumar94 <pardeepkumar@Pardeeps-MacBook-Air.local>
2024-05-11 14:46:33 +0400

    added animation to hrefs

--- src/components/leftSizeNavOptions.tsx
+++ src/components/leftSizeNavOptions.tsx
@@ -2,13 +2,18 @@ import Link from "next/link"
 
 const LeftSizeNavOptions = () => {
 
+    const options = ["About", "Experience", "Projects", "Blogs"]
+
     return (
-        <div className="lg:flex flex-[0.7] flex-col mt-20 hidden">
-            <Link href={"#about"} scroll={true}><p>About</p></Link>
-            <Link href={"#experience"} scroll={true}> <p>Experience</p> </Link>
-            <p>Projects</p>
-            <p>Blogs</p>
-        </div>
+        <ul className="lg:flex flex-[0.7] flex-col mt-20 hidden">
+            {options.map((element, index)=> <li key={index}>
+                <Link className="group flex items-center py-3" href="#experience">
+                    <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                    <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">{element}</span></Link>
+            </li>)}
+            
+        
+        </ul>
     )
 }
 
